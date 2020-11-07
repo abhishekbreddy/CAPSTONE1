0d61caf39a7cad649191bfe8b68e0de027e07205
abhishekbreddy <43873538+abhishekbreddy@users.noreply.github.com>
2020-11-08 00:15:24 +0530

    Update Myprofilestudent.js

--- src/Components/Myprofilestudent.js
+++ src/Components/Myprofilestudent.js
@@ -1,6 +1,97 @@
 import React from 'react'
+import '/home/abhi/Desktop/capstone1/job/src/Components/Pstyle.css'
 export default class Myprofilestudent extends React.Component{
-    render(){
-        return(<h1>shiva</h1>);
-    }
-}
+  constructor()
+  {
+  super()
+    this. state = { 
+      Fullname:null,
+      course:null,
+      Schoolname:null,
+      Collegename:null,
+      Emailid:null,
+      Collegeid:null,
+      Certificate:null,
+      
+  
+        
+        selectedFile: null
+
+  
+  }; 
+        
+}     render() {
+        return(
+          <div className="container bg-dark text-white mt-2">
+<div className = "row">
+<div className="col-sm-8">   
+
+        
+          <div class="form1">
+    
+          <form className="mx-auto px-auto">
+            <p className="h2 font-weight-bold mt-3 mb-3 text-center">Profile</p>
+            <div className="form-group ">
+              <input type="text" placeholder="Full Name" className="form-control"
+              onChange={(e)=>this.setState({
+                ...this.state,
+                Fullname:e.target.value
+              })} 
+              />
+            </div>
+            <div className="form-group mt-2"
+            onChange={(e)=>this.setState({
+              ...this.state,
+              course:e.target.value
+            })}
+            >
+                        <select className="form-control">
+                            <option>Select course</option>
+                            <option>B.tech</option>
+                            <option>b.sc</option>
+                            <option>b.com</option>
+                            <option>m.c.a</option>
+                        </select>
+                    </div>
+            <div className="form-group">
+              <input type="text" placeholder="Schoolname" className="form-control" 
+              onChange={(e)=>this.setState({
+                ...this.state,
+                Schoolname:e.target.value
+              })}
+              />
+              
+            </div>
+            <div className="form-group">
+            <input type="text" placeholder="Collegename" className="form-control" 
+            onChange={(e)=>this.setState({
+              ...this.state,
+              Collegename:e.target.value
+            })}
+            />
+            
+          </div>
+            <div className="form-group">
+              <input type="email" placeholder="Email" className="form-control" 
+              onChange={(e)=>this.setState({
+                ...this.state,
+                email:e.target.value
+              })}
+              />
+            </div>
+    
+            <div className="form-group">
+              <input type="file" className="form-control" 
+              onChange={(e)=>this.setState({
+                ...this.state,
+                Collegeid:e.target.value
+              })}
+              />
+              
+            </div>
+    <input value="SUBMIT" type="submit" className="btn btn-primary font-weight-bold mt-3 mx-auto" onClick={this.getdata} />
+                  
+          
+          </form>
+           </div>
+      </div></div></div> )}}
