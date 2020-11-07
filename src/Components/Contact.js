4c3d99c726d3db481968c249f35e716239c2de48
abhishekbreddy <43873538+abhishekbreddy@users.noreply.github.com>
2020-11-08 00:26:33 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import '/home/abhi/Desktop/capstone1/job/src/Components/cstyle.css'
 export default class Contact extends React.Component{
     render(){
-        return(<h1>shiva</h1>);
+        return(<div className="add"><h1 >Address:</h1>
+        <p>School of Accelerated Learning
+Innov8, Sreshta Marvel, P Janardhan Reddy Nagar, Gachibowli,
+Hyderabad, Telangana 500081</p>
+<h1>Contact no:</h1><p>9440490590</p><h2>email :</h2><p>soal@gmail.com</p>
+</div>);
     }
 }
