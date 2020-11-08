f44c154c9597dc8e607a6b7061dc65c94f812d44
abhishekbreddy <43873538+abhishekbreddy@users.noreply.github.com>
2020-11-08 16:25:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import student from '/home/abhi/Desktop/capstone1/job/src/Components/Student.js'
-//import 'bootstrap' path.
+//import student from '/home/abhi/Desktop/capstone1/job/src/Components/Student.js'
+import Home from '../Components/Home.js';
+import {BrowserRouter, Route ,Switch } from 'react-router-dom'
+
+
+import Student from '../Components/Student'
+import Recruiter from '../Components/Recruiter'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 function App() {
-  return(<div><BrowserRouter>
-    <Recruiter/>
+  return(
+   <div><BrowserRouter>
+  <Student/>
+    </BrowserRouter></div>
     
-    </BrowserRouter>
-    </div>)
+    )
   
 
 }
