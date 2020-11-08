e79dbf8b2ded764a4d3d164bf3cc69e19a3ba840
tabassum231 <67263139+tabassum231@users.noreply.github.com>
2020-11-08 16:27:28 +0530

    Update Recruiter.js

--- src/Components/Recruiter.js
+++ src/Components/Recruiter.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 
-import Applications from '/home/abhi/Desktop/capstone1/job/src/Components/Applications.js'
-import Contact from '/home/abhi/Desktop/capstone1/job/src/Components/Contact.js'
-import Myprofile from '/home/abhi/Desktop/capstone1/job/src/Components/Profilecompletion.js'
-import Signin from '/home/abhi/Desktop/capstone1/job/src/Components/Signin.js'
-import Home from '/home/abhi/Desktop/capstone1/job/src/Components/Home.js'
-import Navigation1 from './Navigation/Navigation1'
+import Applications from '../Components/Applications.js'
+import Contact from '../Components/Contact.js'
+import Myprofile from '../Components/Profilecompletion.js'
+import Signin from '../Components/Signin.js'
+import Home from '../Components/Home.js'
+import Navigation1 from '../Navigation/Navigation1'
 
  class Student extends React.Component{
     render (){
